b42055cae3aecb6de56a107a405cb9f469de0f99
Damien <damiena@gmail.com>
2012-11-03 16:22:24 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Charityhack::Application.config.secret_token = 'b3af40bd8ab98233a5ba1a5ed93bddfaa859a5935fd341841596e927859a3fbfc73998851620ddda64fadd2a3e8dd9a5bdd2090e120a9e0f816f9581e771e0c8'
